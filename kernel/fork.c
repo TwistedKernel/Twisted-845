c5faa716f28ea5c2eb52df4fde32bbde76fbbcc5
Sami Tolvanen <samitolvanen@google.com>
2018-05-02 10:44:59 -0700

    ANDROID: add support for clang Shadow Call Stack (SCS)
    This change adds generic support for clang's Shadow Call Stack, which
    uses a shadow stack to protect return addresses from being overwritten
    by an attacker. Details are available here:
    
      https://clang.llvm.org/docs/ShadowCallStack.html
    
    Bug: 112277034
    Change-Id: Idd553b7c978b0673ab533a68980fb9a654f4510c
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    Signed-off-by: Twisted <36546624+TwistedPrime@users.noreply.github.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -82,6 +82,7 @@
 #include <linux/cpufreq_times.h>
 #include <linux/simple_lmk.h>
 #include <linux/cpu_input_boost.h>
+#include <linux/scs.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -347,6 +348,9 @@ void put_task_stack(struct task_struct *tsk)
 
 void free_task(struct task_struct *tsk)
 {
+	scs_release(tsk);
+	cpufreq_task_times_exit(tsk);
+
 #ifndef CONFIG_THREAD_INFO_IN_TASK
 	/*
 	 * The task is finally done with both the stack and thread_info,
@@ -540,6 +544,7 @@ static struct task_struct *dup_task_struct(struct task_struct *orig, int node)
 	clear_user_return_notifier(tsk);
 	clear_tsk_need_resched(tsk);
 	set_task_stack_end_magic(tsk);
+	scs_task_init(tsk);
 
 #ifdef CONFIG_CC_STACKPROTECTOR
 	tsk->stack_canary = get_random_long();
@@ -1836,6 +1841,9 @@ static __latent_entropy struct task_struct *copy_process(
 	retval = copy_thread_tls(clone_flags, stack_start, stack_size, p, tls);
 	if (retval)
 		goto bad_fork_cleanup_io;
+	retval = scs_prepare(p, node);
+	if (retval)
+		goto bad_fork_cleanup_thread;
 
 	if (pid != &init_struct_pid) {
 		pid = alloc_pid(p->nsproxy->pid_ns_for_children);
