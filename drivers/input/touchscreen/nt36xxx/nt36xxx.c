a2ef4c0ecdbd9a85e23467c65da0900642d18484
Yaroslav Furman <yaro330@gmail.com>
2019-09-12 18:10:42 +0700

    nt36xxx: carefully optimize coordinates reporting
    - Remove an unneeded while loop, CTP_I2C_READ has it's own error checking.
    - Extend XFER_ERROR slightly, sunc input anyway even if we encounter
    an error to avoid issues with touchscreen randomly dying.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Signed-off-by: Twisted Prime <36546624+TwistedPrime@users.noreply.github.com>

--- drivers/input/touchscreen/nt36xxx/nt36xxx.c
+++ drivers/input/touchscreen/nt36xxx/nt36xxx.c
@@ -1080,11 +1080,10 @@ static void nvt_ts_work_func(struct work_struct *work)
 	}
 
 #if WAKEUP_GESTURE
-	if (bTouchIsAwake == 0) {
+	if (unlikely(bTouchIsAwake == 0)) {
 		input_id = (uint8_t)(point_data[1] >> 3);
 		nvt_ts_wakeup_gesture_report(input_id, point_data);
-		enable_irq(ts->client->irq);
-		mutex_unlock(&ts->lock);
+		goto XFER_ERROR;
 		return;
 	}
 #endif
@@ -1170,9 +1169,9 @@ static void nvt_ts_work_func(struct work_struct *work)
 	}
 #endif
 
+XFER_ERROR:
 	input_sync(ts->input_dev);
 
-XFER_ERROR:
 	enable_irq(ts->client->irq);
 
 	mutex_unlock(&ts->lock);
