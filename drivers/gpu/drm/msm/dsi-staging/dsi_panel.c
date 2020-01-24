847a16b48d4cf1b4099a4d907d3bfe3fc4ff1397
twisted <36546624+TwistedPrime@users.noreply.github.com>
2020-01-24 14:13:04 +0000

    Merge Linux 4.9.211
    Signed-off-by: twisted <36546624+TwistedPrime@users.noreply.github.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -1156,6 +1156,11 @@ static int dsi_panel_parse_timing(struct device *parent,
 		goto error;
 	}
 
+	if (mode->refresh_rate > 63) {
+		pr_err("failed to apply mdss-dsi-panel-framerate");
+		mode->refresh_rate *= 2;
+	}
+
 	rc = dsi_panel_parse(of_node, fw_entry,
 		"qcom,mdss-dsi-panel-width", &mode->h_active);
 	if (rc) {
