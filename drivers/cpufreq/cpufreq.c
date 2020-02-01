dbd785c36dcfd924291d851c88a37072a12f80b2
twisted <36546624+TwistedPrime@users.noreply.github.com>
2020-02-01 10:53:26 +0000

    cpufreq: Underclock big cluster to 2.5 GHz
    Based on https://github.com/celtare21/kernel_google_coral/commit/b8bc507779147afef
    
    Signed-off-by: twisted <36546624+TwistedPrime@users.noreply.github.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2349,6 +2349,9 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 	if (ret)
 		return ret;
 
+	if (cpumask_test_cpu(new_policy->cpu, cpu_perf_mask))
+		cpufreq_verify_within_limits(new_policy, new_policy->min, 2553600);
+
 	/* adjust if necessary - all reasons */
 	blocking_notifier_call_chain(&cpufreq_policy_notifier_list,
 			CPUFREQ_ADJUST, new_policy);
