b15794d3368ff3432f9a1357197af22188427a5b
twisted <36546624+TwistedPrime@users.noreply.github.com>
2020-02-03 17:22:02 +0000

    fork.c: Remove devfreq boost usage
    Signed-off-by: twisted <36546624+TwistedPrime@users.noreply.github.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -82,7 +82,6 @@
 #include <linux/cpufreq_times.h>
 #include <linux/simple_lmk.h>
 #include <linux/cpu_input_boost.h>
-#include <linux/devfreq_boost.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -2119,7 +2118,6 @@ long _do_fork(unsigned long clone_flags,
 	/* Boost CPU to the max for 50 ms when userspace launches an app */
 	if (task_is_zygote(current)) {
 		cpu_input_boost_kick_max(50);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
 	}
 
 	/*
