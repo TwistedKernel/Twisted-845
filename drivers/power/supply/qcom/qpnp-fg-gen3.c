7a8f56de39f8217b7264340570603e12ab681cbc
TwistedPrime <36546624+TwistedPrime@users.noreply.github.com>
2020-05-26 20:58:56 +0100

    Revert "qpnp-fg-gen3.c: Add charge boost"
    This reverts commit d6111af7cf2b4d0962d6a7fa3101d2cc80fff39b.

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -26,10 +26,6 @@
 #include <linux/ktime.h>
 #include <linux/qpnp/qpnp-revid.h>
 #include <linux/qpnp/qpnp-misc.h>
-#ifdef CONFIG_CHARGE_BOOST
-#include <linux/cpu_input_boost.h>
-#include <linux/module.h>
-#endif
 #include "fg-core.h"
 #include "fg-reg.h"
 #include "step-chg-jeita.h"
@@ -179,11 +175,6 @@ static void fg_encode_default(struct fg_sram_param *sp,
 	enum fg_sram_param_id id, int val, u8 *buf);
 
 static struct fg_irq_info fg_irqs[FG_IRQ_MAX];
-
-#ifdef CONFIG_CHARGE_BOOST
-bool charge_boost_enabled = true;
-module_param(charge_boost_enabled, bool, 0644);
-#endif
 
 #define PARAM(_id, _addr_word, _addr_byte, _len, _num, _den, _offset,	\
 	      _enc, _dec)						\
@@ -1617,16 +1608,6 @@ static void fg_cap_learning_update(struct fg_chip *chip)
 	bool prime_cc = false;
 
 	mutex_lock(&chip->cl.lock);
-
-#ifdef CONFIG_CHARGE_BOOST
-	if (charge_boost_enabled) {
-	if (chip->charge_status == POWER_SUPPLY_STATUS_CHARGING ||
-	chip->charge_status == POWER_SUPPLY_STATUS_FULL) {
-    	   	cpu_input_boost_kick_max(50000);
-		pr_info("Charge boost kicked in\n");
-	}
-	}
-#endif
 
 	if (!is_temp_valid_cap_learning(chip) || !chip->cl.learned_cc_uah ||
 		chip->battery_missing) {
